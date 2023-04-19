4ef98c5a486cf9e1adaf7d0ed6d612c4854ae16a
nka20 <ntwarilevinkenallan@gmail.com>
2023-04-19 10:41:34 +0200

    js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var a=1/6;
+var b=3/2;
+var c=4/3;
+var d=1/2;
+var e=(a * b) / (c+d);
+//la difference entre let et var est que dès que la variable deja defini on ne pas declarer 'let' avec le meme nom de variable alors que avec 'var' c est possible 
