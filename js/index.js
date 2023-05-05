86f5366d2023ed0689a2c8a34d8f9c98eb1b84e8
nka20 <ntwarilevinkenallan@gmail.com>
2023-05-05 19:03:52 +0200

    let et var

--- js/index.js
+++ js/index.js
@@ -4,5 +4,3 @@ var c=4/3;
 var d=1/2;
 var e=(a * b) / (c+d);
 //la difference entre let et var est que dès que la variable deja defini on ne pas declarer 'let' avec le meme nom de variable
-// alors que avec 'var' c est possible et aussi la portee alors que 'let' peut etre utiliser en dehors d une fonction
-//alors que var si il creer en function ne depasse la function
