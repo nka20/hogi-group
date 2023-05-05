c4b4664a8cc1cdc03ce66974b49b22d0322e8505
nka20 <ntwarilevinkenallan@gmail.com>
2023-05-05 19:24:35 +0200

    redefinition

--- js/index.js
+++ js/index.js
@@ -4,3 +4,6 @@ var c=4/3;
 var d=1/2;
 var e=(a * b) / (c+d);
 //la difference entre let et var est que dès que la variable deja defini on ne pas declarer 'let' avec le meme nom de variable
+// alors que avec 'var' c est possible et aussi la portee alors que 'let'ne  peut etre utiliser en dehors d une condition(if,while,for)
+//alors que var si il creer en function depasse la function
+//mais pour les function ni let ni var ne peut depasser la function
